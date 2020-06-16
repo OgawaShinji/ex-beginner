172d7dedcbdb2fdc2b9b2bf886266e2dde96ca6d
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-06-16 11:33:03 +0900

    create 03controller and add 03.html

---
+++ src/main/java/com/example/controller/Exam03Controller.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ex03")
+public class Exam03Controller {
+
+	@Autowired
+	private ServletContext application;
+	
+	@RequestMapping("")
+	public String index() {
+		return "ex03/exam03";
+	}
+	
+}
